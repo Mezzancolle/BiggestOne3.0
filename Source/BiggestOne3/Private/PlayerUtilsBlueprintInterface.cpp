62deee011885b0608a41df26eaf208c642af3b15
Ximer - Marco <20438890+Ximer612@users.noreply.github.com>
2024-05-24 16:52:36 +0200

    Removed Main Character Ref to all items

---
+++ Source/BiggestOne3/Private/PlayerUtilsBlueprintInterface.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PlayerUtilsBlueprintInterface.h"
+
+// Add default functionality here for any IPlayerUtilsBlueprintInterface functions that are not pure virtual.
