c69ce070cedc555ac8ad5baa7372c2d65c234307
giuliorainoldi <gr.giuliorainoldi@gmail.com>
2025-02-04 16:24:31 +0100

    Fixing compatibility

--- Source/BiggestOne3/BiggestOne3.cpp
+++
@@ -1,6 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "BiggestOne3.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, BiggestOne3, "BiggestOne3" );
--- Source/BiggestOne3/Private/PlayerUtilsBlueprintInterface.cpp
+++
@@ -1,6 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "PlayerUtilsBlueprintInterface.h"
-
-// Add default functionality here for any IPlayerUtilsBlueprintInterface functions that are not pure virtual.
