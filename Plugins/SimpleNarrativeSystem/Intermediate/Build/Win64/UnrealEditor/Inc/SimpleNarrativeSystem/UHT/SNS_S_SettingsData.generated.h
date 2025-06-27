b1fc742b5d08171c972d8e1dc067eea8b1d15552
Ximer - Marco <20438890+Ximer612@users.noreply.github.com>
2025-06-27 15:11:02 +0200

    Pushing binaries

---
+++ Plugins/SimpleNarrativeSystem/Intermediate/Build/Win64/UnrealEditor/Inc/SimpleNarrativeSystem/UHT/SNS_S_SettingsData.generated.h
@@ -0,0 +1,28 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+// IWYU pragma: private, include "Structs/SNS_S_SettingsData.h"
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef SIMPLENARRATIVESYSTEM_SNS_S_SettingsData_generated_h
+#error "SNS_S_SettingsData.generated.h already included, missing '#pragma once' in SNS_S_SettingsData.h"
+#endif
+#define SIMPLENARRATIVESYSTEM_SNS_S_SettingsData_generated_h
+
+#define FID_build_U5M_Marketplace_Sync_LocalBuilds_PluginTemp_HostProject_Plugins_SimpleNarrativeSystem_Source_SimpleNarrativeSystem_Public_Structs_SNS_S_SettingsData_h_14_GENERATED_BODY \
+	friend struct Z_Construct_UScriptStruct_FSNS_S_SettingsData_Statics; \
+	static class UScriptStruct* StaticStruct();
+
+
+template<> SIMPLENARRATIVESYSTEM_API UScriptStruct* StaticStruct<struct FSNS_S_SettingsData>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID FID_build_U5M_Marketplace_Sync_LocalBuilds_PluginTemp_HostProject_Plugins_SimpleNarrativeSystem_Source_SimpleNarrativeSystem_Public_Structs_SNS_S_SettingsData_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
---
+++ Plugins/SimpleNarrativeSystem/Intermediate/Build/Win64/UnrealGame/Inc/SimpleNarrativeSystem/UHT/SNS_CustomProjectSettings.gen.cpp
@@ -0,0 +1,114 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/GeneratedCppIncludes.h"
+#include "SimpleNarrativeSystem/Public/SNS_CustomProjectSettings.h"
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+void EmptyLinkFunctionForGeneratedCodeSNS_CustomProjectSettings() {}
+
+// Begin Cross Module References
+COREUOBJECT_API UClass* Z_Construct_UClass_UClass();
+COREUOBJECT_API UClass* Z_Construct_UClass_UObject();
+SIMPLENARRATIVESYSTEM_API UClass* Z_Construct_UClass_USNS_CustomProjectSettings();
+SIMPLENARRATIVESYSTEM_API UClass* Z_Construct_UClass_USNS_CustomProjectSettings_NoRegister();
+SIMPLENARRATIVESYSTEM_API UClass* Z_Construct_UClass_USNS_Widget_NoRegister();
+UPackage* Z_Construct_UPackage__Script_SimpleNarrativeSystem();
+// End Cross Module References
+
+// Begin Class USNS_CustomProjectSettings
+void USNS_CustomProjectSettings::StaticRegisterNativesUSNS_CustomProjectSettings()
+{
+}
+IMPLEMENT_CLASS_NO_AUTO_REGISTRATION(USNS_CustomProjectSettings);
+UClass* Z_Construct_UClass_USNS_CustomProjectSettings_NoRegister()
+{
+	return USNS_CustomProjectSettings::StaticClass();
+}
+struct Z_Construct_UClass_USNS_CustomProjectSettings_Statics
+{
+#if WITH_METADATA
+	static constexpr UECodeGen_Private::FMetaDataPairParam Class_MetaDataParams[] = {
+		{ "Comment", "/**\n * \n */" },
+		{ "IncludePath", "SNS_CustomProjectSettings.h" },
+		{ "ModuleRelativePath", "Public/SNS_CustomProjectSettings.h" },
+		{ "ObjectInitializerConstructorDeclared", "" },
+	};
+	static constexpr UECodeGen_Private::FMetaDataPairParam NewProp_DialogueWidgetBlueprint_MetaData[] = {
+		{ "Category", "Widget Settings" },
+		{ "DisplayName", "Subtitles Widget" },
+		{ "ModuleRelativePath", "Public/SNS_CustomProjectSettings.h" },
+	};
+	static constexpr UECodeGen_Private::FMetaDataPairParam NewProp_PluginInfo_MetaData[] = {
+		{ "Category", "Info" },
+		{ "DisplayName", "Made by" },
+		{ "ModuleRelativePath", "Public/SNS_CustomProjectSettings.h" },
+	};
+#endif // WITH_METADATA
+	static const UECodeGen_Private::FClassPropertyParams NewProp_DialogueWidgetBlueprint;
+	static const UECodeGen_Private::FStrPropertyParams NewProp_PluginInfo;
+	static const UECodeGen_Private::FPropertyParamsBase* const PropPointers[];
+	static UObject* (*const DependentSingletons[])();
+	static constexpr FCppClassTypeInfoStatic StaticCppClassTypeInfo = {
+		TCppClassTypeTraits<USNS_CustomProjectSettings>::IsAbstract,
+	};
+	static const UECodeGen_Private::FClassParams ClassParams;
+};
+const UECodeGen_Private::FClassPropertyParams Z_Construct_UClass_USNS_CustomProjectSettings_Statics::NewProp_DialogueWidgetBlueprint = { "DialogueWidgetBlueprint", nullptr, (EPropertyFlags)0x0014000000004005, UECodeGen_Private::EPropertyGenFlags::Class, RF_Public|RF_Transient|RF_MarkAsNative, nullptr, nullptr, 1, STRUCT_OFFSET(USNS_CustomProjectSettings, DialogueWidgetBlueprint), Z_Construct_UClass_UClass, Z_Construct_UClass_USNS_Widget_NoRegister, METADATA_PARAMS(UE_ARRAY_COUNT(NewProp_DialogueWidgetBlueprint_MetaData), NewProp_DialogueWidgetBlueprint_MetaData) };
+const UECodeGen_Private::FStrPropertyParams Z_Construct_UClass_USNS_CustomProjectSettings_Statics::NewProp_PluginInfo = { "PluginInfo", nullptr, (EPropertyFlags)0x0010000000034001, UECodeGen_Private::EPropertyGenFlags::Str, RF_Public|RF_Transient|RF_MarkAsNative, nullptr, nullptr, 1, STRUCT_OFFSET(USNS_CustomProjectSettings, PluginInfo), METADATA_PARAMS(UE_ARRAY_COUNT(NewProp_PluginInfo_MetaData), NewProp_PluginInfo_MetaData) };
+const UECodeGen_Private::FPropertyParamsBase* const Z_Construct_UClass_USNS_CustomProjectSettings_Statics::PropPointers[] = {
+	(const UECodeGen_Private::FPropertyParamsBase*)&Z_Construct_UClass_USNS_CustomProjectSettings_Statics::NewProp_DialogueWidgetBlueprint,
+	(const UECodeGen_Private::FPropertyParamsBase*)&Z_Construct_UClass_USNS_CustomProjectSettings_Statics::NewProp_PluginInfo,
+};
+static_assert(UE_ARRAY_COUNT(Z_Construct_UClass_USNS_CustomProjectSettings_Statics::PropPointers) < 2048);
+UObject* (*const Z_Construct_UClass_USNS_CustomProjectSettings_Statics::DependentSingletons[])() = {
+	(UObject* (*)())Z_Construct_UClass_UObject,
+	(UObject* (*)())Z_Construct_UPackage__Script_SimpleNarrativeSystem,
+};
+static_assert(UE_ARRAY_COUNT(Z_Construct_UClass_USNS_CustomProjectSettings_Statics::DependentSingletons) < 16);
+const UECodeGen_Private::FClassParams Z_Construct_UClass_USNS_CustomProjectSettings_Statics::ClassParams = {
+	&USNS_CustomProjectSettings::StaticClass,
+	"SimpleNarrativeSystem",
+	&StaticCppClassTypeInfo,
+	DependentSingletons,
+	nullptr,
+	Z_Construct_UClass_USNS_CustomProjectSettings_Statics::PropPointers,
+	nullptr,
+	UE_ARRAY_COUNT(DependentSingletons),
+	0,
+	UE_ARRAY_COUNT(Z_Construct_UClass_USNS_CustomProjectSettings_Statics::PropPointers),
+	0,
+	0x001000A6u,
+	METADATA_PARAMS(UE_ARRAY_COUNT(Z_Construct_UClass_USNS_CustomProjectSettings_Statics::Class_MetaDataParams), Z_Construct_UClass_USNS_CustomProjectSettings_Statics::Class_MetaDataParams)
+};
+UClass* Z_Construct_UClass_USNS_CustomProjectSettings()
+{
+	if (!Z_Registration_Info_UClass_USNS_CustomProjectSettings.OuterSingleton)
+	{
+		UECodeGen_Private::ConstructUClass(Z_Registration_Info_UClass_USNS_CustomProjectSettings.OuterSingleton, Z_Construct_UClass_USNS_CustomProjectSettings_Statics::ClassParams);
+	}
+	return Z_Registration_Info_UClass_USNS_CustomProjectSettings.OuterSingleton;
+}
+template<> SIMPLENARRATIVESYSTEM_API UClass* StaticClass<USNS_CustomProjectSettings>()
+{
+	return USNS_CustomProjectSettings::StaticClass();
+}
+DEFINE_VTABLE_PTR_HELPER_CTOR(USNS_CustomProjectSettings);
+USNS_CustomProjectSettings::~USNS_CustomProjectSettings() {}
+// End Class USNS_CustomProjectSettings
+
+// Begin Registration
+struct Z_CompiledInDeferFile_FID_build_U5M_Marketplace_Sync_LocalBuilds_PluginTemp_HostProject_Plugins_SimpleNarrativeSystem_Source_SimpleNarrativeSystem_Public_SNS_CustomProjectSettings_h_Statics
+{
+	static constexpr FClassRegisterCompiledInInfo ClassInfo[] = {
+		{ Z_Construct_UClass_USNS_CustomProjectSettings, USNS_CustomProjectSettings::StaticClass, TEXT("USNS_CustomProjectSettings"), &Z_Registration_Info_UClass_USNS_CustomProjectSettings, CONSTRUCT_RELOAD_VERSION_INFO(FClassReloadVersionInfo, sizeof(USNS_CustomProjectSettings), 3710086634U) },
+	};
+};
+static FRegisterCompiledInInfo Z_CompiledInDeferFile_FID_build_U5M_Marketplace_Sync_LocalBuilds_PluginTemp_HostProject_Plugins_SimpleNarrativeSystem_Source_SimpleNarrativeSystem_Public_SNS_CustomProjectSettings_h_2128841271(TEXT("/Script/SimpleNarrativeSystem"),
+	Z_CompiledInDeferFile_FID_build_U5M_Marketplace_Sync_LocalBuilds_PluginTemp_HostProject_Plugins_SimpleNarrativeSystem_Source_SimpleNarrativeSystem_Public_SNS_CustomProjectSettings_h_Statics::ClassInfo, UE_ARRAY_COUNT(Z_CompiledInDeferFile_FID_build_U5M_Marketplace_Sync_LocalBuilds_PluginTemp_HostProject_Plugins_SimpleNarrativeSystem_Source_SimpleNarrativeSystem_Public_SNS_CustomProjectSettings_h_Statics::ClassInfo),
+	nullptr, 0,
+	nullptr, 0);
+// End Registration
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
